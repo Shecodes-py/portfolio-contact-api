b202fea01514b0ab49116c8abf339653c9f18569
Ella Udotong <94646178+Shecodes-py@users.noreply.github.com>
2026-01-19 16:23:34 +0100

    Update CORS_ALLOWED_ORIGINS to include additional domains and enable credentials support

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -162,6 +162,8 @@ CORS_ALLOWED_ORIGINS = [
     "http://peace-udotong.vercel.app",
     'http://www.peace-udotong.vercel.app',
     'https://peace-udotong.vercel.app',
+    'portfolio-contact-api-8iln.onrender.com', 
+    'peace-udotong.vercel.app',
 ]
 
 CORS_ALLOW_CREDENTIALS = True
